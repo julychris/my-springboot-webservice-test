4e9c01337f7c3b550bf10415f9a0db21c33d5f51
julychris <jjang.gle84@gmail.com>
2022-03-11 02:42:01 +0900

    테스트용 commit

--- src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -35,7 +35,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
         User user = saveOrUpdate(attributes);
 
-        httpSession.setAttribute("user", new SessionUser(user));
+        httpSession.setAttribute("user",new SessionUser(user));
 
         return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())), attributes.getAttributes(), attributes.getNameAttributeKey());
     }
