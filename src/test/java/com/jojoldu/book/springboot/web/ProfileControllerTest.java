dd094b5653744e9cafff4f3a2d17b01b9e7343fe
julychris <jjang.gle84@gmail.com>
2022-03-12 19:04:23 +0900

    10.2 까지 하고 commit(오타 수정)

--- src/test/java/com/jojoldu/book/springboot/web/ProfileControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/ProfileControllerTest.java
@@ -2,6 +2,7 @@ package com.jojoldu.book.springboot.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -18,8 +19,10 @@ public class ProfileControllerTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
     private TestRestTemplate restTemplate;
 
+    @Test
     public void profile은_인증없이_호출된다() throws Exception {
         String expected = "default";
 
